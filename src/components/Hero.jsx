ec1384dd32073088710933ec1a988ac95dcb78d2
Tav <therzlic@cisco.com>
2023-06-22 14:24:13 +0300

    video dimensions

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,4 @@
 import { motion } from 'framer-motion';
-
 import { styles } from '../styles';
 import { ComputersCanvas, JavaCanvas } from './canvas';
 import { tav } from '../assets';
@@ -7,14 +6,19 @@ import { tav } from '../assets';
 import React, { useState, useEffect } from "react";
 
 const Hero = () => {
+  let initialWidth = window.innerWidth <= 500 ? window.innerWidth :
+                     window.innerWidth <= 720 ? 500 :
+                     window.innerWidth <= 1800 ? 500 : 1280;
   const [iframeDimensions, setIframeDimensions] = useState({
-    width: window.innerWidth > 1280 ? 1280 : window.innerWidth,
-    height: ((window.innerWidth > 1280 ? 1280 : window.innerWidth) / 16) * 9
+    width: initialWidth,
+    height: (initialWidth / 16) * 9
   });
 
   useEffect(() => {
     function handleResize() {
-      const newWidth = window.innerWidth > 1280 ? 1280 : window.innerWidth;
+      let newWidth = window.innerWidth <= 500 ? window.innerWidth :
+                     window.innerWidth <= 720 ? 500 :
+                     window.innerWidth <= 1800 ? 720 : 1280;
       setIframeDimensions({
         width: newWidth,
         height: (newWidth / 16) * 9
@@ -34,15 +38,10 @@ const Hero = () => {
 
   return (
     <section className="relative w-full h-screen mx-auto">
-      <div className={`${styles.paddingX} absolute
-      inset-0 top-[120px] max-w-7xl mx-auto flex
-      flex-row items-start gap-5`}>
-        <div className="flex flex-col
-        justify-center items-center mt-5">
-          <div className="w-5 h-5 rounded-full
-          bg-[#915eff]"/>
-          <div className="w-1 sm:5-80 h-40
-          violet-gradient" />
+      <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
+          <div className="w-1 sm:5-80 h-40 violet-gradient" />
         </div>
 
         <div>
@@ -58,7 +57,7 @@ const Hero = () => {
         </div>
       </div>
 
-      <div className="absolute inset-0 flex justify-center items-center" style={{ marginTop: '100px' }}>
+      <div className="absolute inset-0 flex justify-center items-center" style={{ marginTop: '200px' }}>
         <iframe 
           width={iframeDimensions.width} 
           height={iframeDimensions.height} 
@@ -72,14 +71,10 @@ const Hero = () => {
 
       {/* {RandomComponent} */} // Random 3D model, unnecessary when the video is present
 
-      <div className="absolute xs:bottom-10 
-      bottom-32 w-full flex justify-center
-      items-center">
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
         <a href="#about">
-          <div className="w-[35px] h-[64px]
-          rounded-3xl border-4 border-secondary
-          flex justify-center items-start p-2">
-            <motion.dev 
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div 
               animate={{
                 y: [0, 24, 0]
               }}
@@ -88,8 +83,7 @@ const Hero = () => {
                 repeat: Infinity,
                 repeatType: 'loop'
               }}
-              className="w-3 h-3 rounded-full
-              bg-secondary mb-1"
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
@@ -99,4 +93,3 @@ const Hero = () => {
 }
 
 export default Hero;
-
