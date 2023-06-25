931c6b1aa40eacfaf521e0120adf378e85e17443
Tav <therzlic@cisco.com>
2023-06-25 11:37:25 +0300

    positioning

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,11 +30,13 @@ const Hero = () => {
         </div>
       </div>
 
+      <br/>
+      <br/>
+      <br />
+      {RandomComponent}      
 
     
-      {RandomComponent}
-    
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute xs:bottom-10 bottom-0 w-full flex justify-center items-center">
         <a href="#work">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div 
